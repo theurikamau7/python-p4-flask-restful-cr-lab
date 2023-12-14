243a14c9864c6fee1fa17fa99ec2266df64ceecb
IanKamau <theurikamau7@gmail.com>
2023-12-14 21:59:31 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,27 +1,62 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
-from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
 
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-db.init_app(app)
 
-api = Api(app)
 
-class Plants(Resource):
-    pass
+class Plant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    price = db.Column(db.Float)
 
-class PlantByID(Resource):
-    pass
-        
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': self.price
+        }
+
+
+@app.route('/plants', methods=['GET'])
+def get_plants():
+    plants = Plant.query.all()
+    plant_list = [plant.to_dict() for plant in plants]
+    return jsonify(plant_list)
+
+
+@app.route('/plants', methods=['POST'])
+def create_plant():
+    data = request.get_json()
+    name = data.get('name')
+    image = data.get('image')
+    price = data.get('price')
+
+    plant = Plant(name=name, image=image, price=price)
+    db.session.add(plant)
+    db.session.commit()
+
+    return jsonify(plant.to_dict()), 201
+
+
+@app.route('/plants/<int:plant_id>', methods=['GET'])
+def get_plant(plant_id):
+    plant = Plant.query.get(plant_id)
+    if plant:
+        return jsonify(plant.to_dict())
+    else:
+        return jsonify({'message': 'Plant not found'}), 404
+
 
 if __name__ == '__main__':
+    db.init_app(app)  # Initialize the SQLAlchemy app
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'Plant(name={self.name}, image={self.image}, price={self.price})'
